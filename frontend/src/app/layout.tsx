17367b2f062c725c57a0e27e5ff7e795906d0220
oarthurfc <arthuryouface@gmail.com>
2025-05-23 22:38:44 -0300

    feat: adding home page header

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const interSans = Inter({
   variable: "--font-inter-sans",
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body
         className={`${interSans.variable} antialiased`}
       >
+        <Header />
         {children}
       </body>
     </html>
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import Link from "next/link"
+
+export default function Header() {
+  return (
+    <div className="flex mx-auto py-3 justify-around bg-white">
+        <div className="flex max-w-[980px] w-full justify-between items-center">
+            <h4 className="text-sm font-semibold">QualMacBookComprar</h4>
+            <nav>
+                <ul className="flex flex-row gap-4">
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/sobre">Sobre</Link>
+                    </li>
+                    <li>
+                        <Link href="/contato">Contato</Link>
+                    </li>
+                </ul>
+            </nav>
+            <div className="flex flex-row gap-4">
+                <span>icon</span>
+                <span>icon</span>
+            </div>
+        </div>
+    </div>
+  )
+}
