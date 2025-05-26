6984c309117f180e7eb3e7614b094ad13fcf8205
oarthurfc <arthuryouface@gmail.com>
2025-05-26 11:28:14 -0300

    feat: adding title gradient

--- frontend/src/app/_components/HeroSection.tsx
+++ frontend/src/app/_components/HeroSection.tsx
@@ -4,7 +4,9 @@ import Image from "next/image";
 export default function HeroSection() {
   return (
     <main className="flex flex-col min-h-screen mb-8 justify-center items-center text-center gap-14">
-      <h1 className="text-5xl font-bold text-secondary max-w-lg">Qual MacBook comprar em 2025?</h1>
+      <h1 className="text-5xl font-bold max-w-lg bg-gradient-to-r from-[#022640] to-[#5786AB] bg-clip-text text-transparent">
+        Qual MacBook comprar em 2025?
+      </h1>
       <Image src="/hero-macbook.png" alt="Logo" width={694} height={332} />
       <div className="flex flex-col max-w-md items-center gap-4">
         <p className="text-foreground text-xl font-semibold gap-4">
